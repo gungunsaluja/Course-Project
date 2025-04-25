7c08a299b3ab33b3141bd39690602c89dacb172b
Gungun Saluja <gungunsaluja16@gmail.com>
2025-04-26 02:43:32 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const User = require('../models/User');
+const User = require('../models/user');
 
 // Google Strategy
 passport.use(new GoogleStrategy({
